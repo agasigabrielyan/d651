c3247595f7c85717531c2296dad96a7f356ee443
agasi <agasi79@list.ru>
2023-07-22 17:54:35 +0300

    Протестируеим работу гита изменили файл index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle("Главная");
 ?>
+<h2>ПРОВЕРКА РАБОТЫ ГИТА</h2>
     <script src="<?=SITE_TEMPLATE_PATH?>/src/lib/script.js"></script>
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
